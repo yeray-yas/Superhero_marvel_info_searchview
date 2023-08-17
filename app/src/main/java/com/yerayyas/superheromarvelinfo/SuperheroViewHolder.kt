0a74f24e2f2fc71d7832e1770c5bf44bd840015a
yeray-yas <yerayarucas@gmail.com>
2023-08-17 21:35:24 +0100

    show images on cardview's items

--- app/src/main/java/com/yerayyas/superheromarvelinfo/SuperheroViewHolder.kt
+++ app/src/main/java/com/yerayyas/superheromarvelinfo/SuperheroViewHolder.kt
@@ -2,6 +2,7 @@ package com.yerayyas.superheromarvelinfo
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.squareup.picasso.Picasso
 import com.yerayyas.superheromarvelinfo.data.model.SuperheroItemResponse
 import com.yerayyas.superheromarvelinfo.databinding.ItemSuperheroBinding
 
@@ -12,9 +13,14 @@ class SuperheroViewHolder(view: View) : ViewHolder(view) {
     fun bind(superheroItemResponse: SuperheroItemResponse) {
 
         binding.tvSuperheroName.text = superheroItemResponse.name
+        val imageUrl =
+            "${superheroItemResponse.thumbnail.path}.${superheroItemResponse.thumbnail.extension}"
 
+        if (imageUrl.contains("image_not_available")) {
+            Picasso.get().load(R.drawable.marvel_image_not_found).into(binding.ivSuperhero)
 
-
+        } else {
+            Picasso.get().load(imageUrl).into(binding.ivSuperhero)
+        }
     }
-
 }
